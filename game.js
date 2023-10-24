2d944fffb7f6335a5234c6ab9b8b8d56a634b2bf
Wilmer19 <40205328+Wilmer19@users.noreply.github.com>
2023-10-24 15:02:32 -0400

    Changed the background color to green

--- game.js
+++ game.js
@@ -82,7 +82,7 @@ class mainScene {
   new Phaser.Game({
     width: 700, // Width of the game in pixels
     height: 400, // Height of the game in pixels
-    backgroundColor: '#3498db', // The background color (blue)
+    backgroundColor: '#50C878', // The background color (green)
     scene: mainScene, // The name of the scene we created
     physics: { default: 'arcade' }, // The physics engine to use
     parent: 'game', // Create the game inside the <div id="game"> 
